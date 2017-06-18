1629b16bf44187c82f89df5c8ec1fee40258ed05
Matvey Arye <mat@timescale.com>
2017-06-18 16:06:49 -0400

    Added hypertable-level chunk cache for better performance

--- src/hypertable.c
+++ src/hypertable.c
@@ -5,6 +5,7 @@
 
 #include "hypertable.h"
 #include "dimension.h"
+#include "chunk.h"
 
 Hypertable *
 hypertable_from_tuple(HeapTuple tuple)
@@ -17,6 +18,7 @@ hypertable_from_tuple(HeapTuple tuple)
 	namespace_oid = get_namespace_oid(NameStr(h->fd.schema_name), false);
 	h->main_table_relid = get_relname_relid(NameStr(h->fd.table_name), namespace_oid);
     h->space = dimension_scan(h->fd.id, h->main_table_relid);
+	h->chunk_cache = subspace_store_init(h->space->num_closed_dimensions + h->space->num_open_dimensions);
 
 	return h;
 }
@@ -39,4 +41,24 @@ hypertable_get_closed_dimension(Hypertable *h)
     
 	Assert(h->space->num_closed_dimensions == 1);
 	return h->space->closed_dimensions[0];
+}
+
+Chunk *hypertable_get_chunk(Hypertable *h, Point *point) 
+{
+ 	Chunk * chunk = subspace_store_get(h->chunk_cache, point);
+
+	if (NULL == chunk) 
+	{
+		Hypercube *hc;
+		chunk = chunk_get_or_create(h->space, point);
+		
+		if (NULL == chunk)
+			elog(ERROR, "No chunk found or created");
+
+		chunk_constraint_scan(chunk);
+		hc = hypercube_from_constraints(chunk->constraints, chunk->num_constraints);
+        subspace_store_add(h->chunk_cache, hc, chunk, pfree);
+	}
+
+	return chunk;
 }
